fd87134282dc40a4d1f96f6f12b38b45b2353b93
beordle <beordle@Mac-74d435c7efd2.lan>
2015-08-22 21:35:47 +0800

    xchg 交换变量

--- Base/cpp 嵌入 asm.cpp
+++ Base/cpp 嵌入 asm.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    asm (
-        "nop;" 
-        "nop;"
-    );      
+    int a=8, b=1;
+
+    asm ("xchg %1, %0;"
+        // "nop;"
+
+             :"=r"(b),"=r"(a)    
+             :"r"(b),"r"(a)        
+             );      
+
+    printf("Result: %d, %d\n", a, b);
+
     return 0;
 }

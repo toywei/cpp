74b10ca44f1a9135f5e9df3b7c3f94082ffa373f
beordle <beordle@Mac-74d435c7efd2.lan>
2015-08-23 19:07:57 +0800

    更新汇编

--- Base/cpp 嵌入 asm.cpp
+++ Base/cpp 嵌入 asm.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 using namespace std;
+int sum(int times){
+    int sumval=0;
+    __asm__ __volatile__(
+            "xorl %%eax, %%eax;" //a=0
+            "xorl %%ecx, %%ecx;" //c=0
+            "movl %0,%%ebx;"  //b=sumval
+            "loop:"
+            "incl %%ecx;"        //c++
+            "addl %%ecx,%%eax;"  //a+=c
+            "cmpl %%ebx,%%ecx;"   // b vs c?
+            "jl loop;"            // < goto loop
+            "movl %%eax,%0;"
+             :"=r"(sumval)
+             :"r"(times)
+             :"eax","ecx" ,"ebx"       
+             );
+    return sumval;
+}
+
+void swap(int a,int b){
+    __asm__ __volatile__("xchg %1, %0;"  //x86 only
+             :"=r"(b),"=r"(a)
+             :"r"(b),"r"(a)     
+             );
+    cout<<a<<","<<b<<endl;
+}
+
 int main(){
-    int a=8, b=1;
 
-    asm ("xchg %1, %0;"
-        // "nop;"
 
-             :"=r"(b),"=r"(a)    
-             :"r"(b),"r"(a)        
-             );      
-
-    printf("Result: %d, %d\n", a, b);
-
+    swap(1,8);
+    cout<<sum(100);
     return 0;
 }
